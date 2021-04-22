3b7780fa0c310a1415c1d8a8e38bfc6cd3581fa6
xiamubobby <xiamubobby@gmail.com>
2021-04-22 12:06:03 +0800

    Update build.sh
    fix dynamic $lib_ext under Linux

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,6 +1,5 @@
 rm -rf build
 cmake CMakeLists.txt -B build
 cmake --build build --config Release
-lib_ext=".so"
-lib_ext=$([[ $(uname) = "Darwin" ]] && echo ".dylib")
+lib_ext=$([[ "$(uname -s)" = "Darwin"  ]] && echo ".dylib" || echo ".so")
 cp "build/lib/libjxtp${lib_ext}" ../lib
